81886d299b5ff1af6c7443bdf905f8d7f97a94a0
dnyang-lab <105845194+dnyang-lab@users.noreply.github.com>
2022-07-14 00:27:53 +0800

    Add files via upload

---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,156 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin_4.x-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Release
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/Baseline.o \
+	${OBJECTDIR}/DP_based.o \
+	${OBJECTDIR}/Deployment.o \
+	${OBJECTDIR}/EdgeCover.o \
+	${OBJECTDIR}/GreedyAllocation.o \
+	${OBJECTDIR}/Initial.o \
+	${OBJECTDIR}/MAMC.o \
+	${OBJECTDIR}/MCS.o \
+	${OBJECTDIR}/Random.o \
+	${OBJECTDIR}/RoundRobin.o \
+	${OBJECTDIR}/SINR.o \
+	${OBJECTDIR}/TMC2013.o \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lte-sim-20150728.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lte-sim-20150728.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lte-sim-20150728 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/Baseline.o: Baseline.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Baseline.o Baseline.c
+
+${OBJECTDIR}/DP_based.o: DP_based.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DP_based.o DP_based.c
+
+${OBJECTDIR}/Deployment.o: Deployment.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Deployment.o Deployment.c
+
+${OBJECTDIR}/EdgeCover.o: EdgeCover.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/EdgeCover.o EdgeCover.c
+
+${OBJECTDIR}/GreedyAllocation.o: GreedyAllocation.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/GreedyAllocation.o GreedyAllocation.c
+
+${OBJECTDIR}/Initial.o: Initial.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Initial.o Initial.c
+
+${OBJECTDIR}/MAMC.o: MAMC.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/MAMC.o MAMC.c
+
+${OBJECTDIR}/MCS.o: MCS.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/MCS.o MCS.c
+
+${OBJECTDIR}/Random.o: Random.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Random.o Random.c
+
+${OBJECTDIR}/RoundRobin.o: RoundRobin.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/RoundRobin.o RoundRobin.c
+
+${OBJECTDIR}/SINR.o: SINR.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/SINR.o SINR.c
+
+${OBJECTDIR}/TMC2013.o: TMC2013.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/TMC2013.o TMC2013.c
+
+${OBJECTDIR}/main.o: main.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/lte-sim-20150728.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
